2d00f9897c46b6a4e64f7ff199c349ca328e59f2
bugseekor <compukin@gmail.com>
2017-12-20 11:48:01 -0500

    SecondCommit

--- app/src/main/java/com/example/spark1435/midterm/MainActivity.java
+++ app/src/main/java/com/example/spark1435/midterm/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.spark1435.midterm;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,10 +19,18 @@ public class MainActivity extends AppCompatActivity {
 
         switch (view.getId()) {
             case R.id.addCommentButton:
+                Context context = getApplicationContext();
+                CharSequence text = "Hello toast!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
                 sendMessage1(view);
                 break;
             case R.id.contactButton:
-
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.addCategory(Intent.CATEGORY_APP_MESSAGING);
+                startActivity(intent);
                 break;
         }
     }
